662a8eb0a3e45e9009fd5438ecb95af735dbffac
Jakub Mazur <jakmaz.dev@icloud.com>
2025-06-10 15:03:50 +0200

    style: improve blogs page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -32,9 +32,30 @@ const blogPosts = [
   {
     image: "/heatmap.png",
     title: "Blog Post Title 5",
-    description: "This is a short description for blog post 5. It gives a brief overview of the content.",
-    date: "June 30, 2025",
+    description: "This is a short description for blog post 1. It gives a brief overview of the content.",
+    date: "June 10, 2025",
     link: "/blog/post-5",
+  },
+  {
+    image: "/heatmap.png",
+    title: "Blog Post Title 6",
+    description: "This is a short description for blog post 2. It gives a brief overview of the content.",
+    date: "June 15, 2025",
+    link: "/blog/post-6",
+  },
+  {
+    image: "/heatmap.png",
+    title: "Blog Post Title 7",
+    description: "This is a short description for blog post 3. It gives a brief overview of the content.",
+    date: "June 20, 2025",
+    link: "/blog/post-7",
+  },
+  {
+    image: "/heatmap.png",
+    title: "Blog Post Title 8",
+    description: "This is a short description for blog post 4. It gives a brief overview of the content.",
+    date: "June 25, 2025",
+    link: "/blog/post-8",
   },
 ];
 
@@ -42,15 +63,14 @@ export default function BlogPage() {
   return (
     <div className="flex flex-col ">
       <div className="flex-grow container mx-auto px-4 py-20 pt-[72px]">
-        <h1 className="text-4xl font-bold mb-8">Writings</h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <h1 className="text-4xl font-bold mb-8">Blog</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {blogPosts.map((post) => (
             <BlogPageCard
               key={post.title}
               image={post.image}
               title={post.title}
               description={post.description}
-              date={post.date}
               link={post.link}
             />
           ))}
--- src/components/blog/blog-post-card.tsx
+++ src/components/blog/blog-post-card.tsx
@@ -1,25 +1,70 @@
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreVertical } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface BlogPostCardProps {
   image: string;
   title: string;
   description: string;
-  date: string;
   link: string;
 }
 
-export function BlogPageCard({ image, title, description, date, link }: BlogPostCardProps) {
+export function BlogPageCard({
+  image,
+  title,
+  description,
+  link,
+}: BlogPostCardProps) {
   return (
-    <Link href={link} className="block bg-card  hover:bg-card/80 transition-colors group">
-      <div className="relative w-full h-48 mb-4">
-        <Image src={image} alt={title} layout="fill" objectFit="cover" />
-      </div>
-      <div className="p-4">
-        <h3 className="text-xl font-mono text-white mb-2 leading-tight">{title}</h3>
-        <p className="text-sm text-gray-400 mb-4">{description}</p>
-        <p className="text-xs text-gray-500">{date}</p>
-      </div>
-    </Link>
+    <div className="block bg-bg3 hover:bg-bg3/80 transition-colors group relative">
+
+      <Link href={link}>
+        <div className="relative w-full h-48 bg-black">
+          <Image src={image} alt={title} layout="fill" objectFit="cover" />
+        </div>
+        <div className="p-4">
+          <div className="flex items-center justify-between">
+            <h3 className="text-white">{title}</h3>
+            <Dropdown />
+          </div>
+          <p className="text-sm text-gray-400 mb-8">{description}</p>
+          <p className="text-xs text-blue1">Text link</p>
+
+        </div>
+      </Link>
+    </div>
   );
-} 
+}
+
+function Dropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="default"
+          className="bg-bg4 hover:bg-bg4/80 text-white mb-2 h-8 w-8"
+        >
+          <MoreVertical className="w-4 h-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-32 bg-bg4 border-none">
+        <DropdownMenuItem className="cursor-pointer focus:bg-white/10">
+          Download
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer focus:bg-white/10">
+          Save for later
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer focus:bg-white/10">
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
