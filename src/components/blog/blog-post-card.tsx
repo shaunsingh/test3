c655918e9d8df37facf5bff27ef5c738468168db
Jakub Mazur <jakmaz.dev@icloud.com>
2025-06-10 17:45:47 +0200

    fix: remove post dropdown

--- src/components/blog/blog-post-card.tsx
+++ src/components/blog/blog-post-card.tsx
@@ -1,11 +1,3 @@
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { MoreVertical } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -38,40 +30,12 @@ export function BlogPageCard({
           <Image src={image} alt={title} layout="fill" objectFit="cover" />
         </div>
         <div className="p-4">
-          <div className="flex items-center justify-between">
-            <h3 className="text-white">{title}</h3>
-            <Dropdown />
-          </div>
-          <p className="text-sm text-fg1 mb-8">{description}</p>
-          <p className="text-xs text-fg1">{formattedDate}</p>
+          <h3 className="text-fg3 mb-2">{title}</h3>
+          <p className="text-sm text-fg1 mb-6">{description}</p>
+          <p className="text-xs text-fg1/50">{formattedDate}</p>
         </div>
       </Link>
     </div>
   );
 }
 
-function Dropdown() {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="default"
-          className="bg-bg3 hover:bg-bg4 text-white mb-2 h-8 w-8"
-        >
-          <MoreVertical className="w-4 h-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-32 bg-bg4 border-none">
-        <DropdownMenuItem className="cursor-pointer focus:bg-white/10">
-          Download
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer focus:bg-white/10">
-          Save for later
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer focus:bg-white/10">
-          Delete
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-}
