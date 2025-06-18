fbe2ab6df26958dd2e50ffe225b9c3a5c0d1c81b
shaunsingh <shauryasingh@nyoom.engineering>
2025-06-18 11:10:45 -0400

    make 404 mobile responsive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,21 +2,21 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-theme(spacing.20)*6)] text-center bg-background text-white px-4 py-8">
-      <h1 className="relative text-6xl font-bold mb-4 font-mono">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-theme(spacing.16))] text-center bg-background text-white px-4 py-8">
+      <h1 className="relative text-9xl font-bold mb-4 font-mono">
         40
         <span className="relative inline-block">
           4
           <span
             role="img"
             aria-label="angry symbol"
-            className="absolute -top-3 -right-1 text-red-600 text-3xl rotate-15"
+            className="absolute -top-0.5 right-1 text-red-600 text-4xl rotate-[15deg]"
           >
             💢
           </span>
         </span>
       </h1>
-      <h2 className="text-2xl font-semibold mb-8 font-mono">Page Not Found</h2>
+      <h2 className="text-3xl md:text-4xl font-semibold mb-8 font-mono">Page Not Found</h2>
       <Link href="/" className="bg-white text-black px-6 py-3 rounded-md font-medium hover:bg-gray-200 transition-colors font-mono">
         Go to Homepage
       </Link>
